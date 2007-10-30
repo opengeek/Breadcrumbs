d88a42290e16f29137da8fbf6c5e21a36ecefe01
opengeek <opengeek@24346d65-fb89-4855-a768-f5fb827ddd76>
2007-10-30 02:42:28 +0000

    * Missing parenthesis added

--- assets/snippets/breadcrumbs/breadcrumbs.class.php
+++ assets/snippets/breadcrumbs/breadcrumbs.class.php
@@ -1,7 +1,7 @@
 <?php
 class BreadCrumbs {
 	var $crumbs;
-	
+
 	function BreadCrumbs(&$modx,$config) {
 		$this->__construct($modx,$config);
 	}
@@ -10,35 +10,35 @@ class BreadCrumbs {
 		$this->config = $config;
 		$this->crumbs = array();
 	}
-	
+
 	function showCurrentPage($resource) {
 		// show current page, as link or not
 		if ($this->config['showCurrentCrumb']) {
-		
-			$titleToShow = $resource->get($this->config['titleField']) 
+
+			$titleToShow = $resource->get($this->config['titleField'])
 				? $resource->get($this->config['titleField'])
 				: $resource->pagetitle;
-			
+
 			if ($this->config['currentAsLink'] && (!$this->config['respectHidemenu'] || ($this->config['respectHidemenu'] && $resource->hidemenu != 1 ))) {
-			
-				$descriptionToUse = ($resource->get($this->config['descField'])
+
+				$descriptionToUse = ($resource->get($this->config['descField']))
 					? $resource->get($this->config['descField'])
 					: $resource->pagetitle;
 				$this->crumbs[] = '<a class="B_currentCrumb" href="[~'.$this->modx->documentIdentifier.'~]" title="'.$descriptionToUse.'">'.$titleToShow.'</a>';
-				
+
 			} else {
 				$this->crumbs[] = '<span class="B_currentCrumb">'.$resource->pagetitle.'</span>';
 			}
 		}
 	}
-	
+
 	function getMiddleCrumbs($resource_id,&$count) {
-		// insert '...' if maximum number of crumbs exceded	
+		// insert '...' if maximum number of crumbs exceded
 		if ($count >= $this->config['maxCrumbs']) {
 			$this->crumbs[] = '<span class="B_hideCrumb">...</span>';
 			return false;
 		}
-		
+
 		$wa = array(
 			'id' => $resource_id,
 		);
@@ -47,46 +47,46 @@ class BreadCrumbs {
 			$wa['deleted'] = 0;
 		}
 		$parent = $this->modx->getObject('modResource',$wa);
-		
+
 		if ($parent->id != $this->modx->config['site_start']) {
-			if (!$this->config['respectHidemenu'] || ($this->config['respectHidemenu'] && $parent['hidemenu'] != 1)) { 
+			if (!$this->config['respectHidemenu'] || ($this->config['respectHidemenu'] && $parent['hidemenu'] != 1)) {
 				$titleToShow = $parent->get($this->config['titleField'])
 					? $parent->get($this->config['titleField'])
 					: $parent->pagetitle;
 				$descriptionToUse = $parent->get($this->config['descField'])
 					? $parent->get($this->config['descField'])
 					: $parent->pagetitle;
-				
-				$this->crumbs[] = '<a class="B_crumb" href="[~'.$parent->id.'~]" title="'.$descriptionToUse.'">'.$titleToShow.'</a>'; 
+
+				$this->crumbs[] = '<a class="B_crumb" href="[~'.$parent->id.'~]" title="'.$descriptionToUse.'">'.$titleToShow.'</a>';
 			}
-		} // end if 
-		
+		} // end if
+
 		$count++;
 		if ($parent->parent != 0) {
 			$this->getMiddleCrumbs($parent->parent,$count);
 		}
 	}
-	
+
 	function load() {
 		if ($this->config['showCrumbsAtHome']
 		|| ($this->modx->documentIdentifier == $this->modx->config['site_start'])) return false;
-		
+
 		// get current resource parent info
 		$resource = $this->modx->getObject('modResource',$this->modx->documentIdentifier);
-				
+
 		// assemble intermediate crumbs
 		$crumbCount = 0;
 		$this->getMiddleCrumbs($resource->id,$crumbCount);
-		
-		// add home link if desired		
+
+		// add home link if desired
 		if ($this->config['showHomeCrumb'] && ($this->modx->documentIdentifier != $this->modx->config['site_start'])) {
-			$this->crumbs[] = '<a class="B_homeCrumb" href="[~'.$this->modx->config['site_start'].'~]" title="'.$this->config['homeCrumbDescription'].'">'.$this->config['homeCrumbTitle'].'</a>'; 
+			$this->crumbs[] = '<a class="B_homeCrumb" href="[~'.$this->modx->config['site_start'].'~]" title="'.$this->config['homeCrumbDescription'].'">'.$this->config['homeCrumbTitle'].'</a>';
 		}
-		
-		$this->crumbs = array_reverse($this->crumbs); 
-		$this->crumbs[0] = '<span class="B_firstCrumb">'.$this->crumbs[0].'</span>'; 
+
+		$this->crumbs = array_reverse($this->crumbs);
+		$this->crumbs[0] = '<span class="B_firstCrumb">'.$this->crumbs[0].'</span>';
 		$this->crumbs[count($this->crumbs)-1] = '<span class="B_lastCrumb">'.$this->crumbs[count($this->crumbs)-1].'</span>';
-		
+
 		return '<span class="B_crumbBox">'. join($this->crumbs, ' '.$this->config['crumbSeparator'].' ').'</span>';
 	}
 }
